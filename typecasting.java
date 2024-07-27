a115f6bf081e1fc622d31580acf79f7c91f7a789
TharunG2005 <140417264+TharunG2005@users.noreply.github.com>
2024-07-27 15:53:22 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,13 @@
+public class typecasting {
+    public static void main(String[]args){
+        double mydouble = 9.78d;
+        int myint = (int) mydouble;
+        int my=9;
+        double myd=my;
+        System.out.println(my);
+        System.out.println(myd);
+        System.out.println(mydouble);
+        System.out.println(myint);
+    }
+    
+}
