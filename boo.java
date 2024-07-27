5c4afcea0a9ab03352306a9f524d6ff0217af82f
TharunG2005 <140417264+TharunG2005@users.noreply.github.com>
2024-07-27 16:57:24 +0530

    Add files via upload

---
+++ boo.java
@@ -0,0 +1,19 @@
+public class boo {
+    public static void main(String[] args){
+        int x=10;
+        int y=9;
+        System.out.println(x>y);
+        System.out.println(10==15);
+        if(20>18){
+            System.out.println("20 is greater than 18");
+        }
+        int time=20;
+        if(time<18){
+            System.out.println("GOOD DAY");
+        }
+        else{
+            System.out.println("GOOD EVENING");
+        }
+    }
+    
+}
